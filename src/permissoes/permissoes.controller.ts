dbf4a0f7401f7f0d607e0aa03e2716f74c0f276b
Guilherme Ancheschi Werneck Pereira <guilhermewerneckpereira11@hotmail.com>
2026-04-01 09:57:11 -0300

    ✨ feat(master): Aplica controle de permissões no módulo de permissões.

--- src/permissoes/permissoes.controller.ts
+++ src/permissoes/permissoes.controller.ts
@@ -9,13 +9,15 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { AdminGuard } from '../auth/admin.guard';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Permissions } from '../auth/permissions.decorator';
+import { PermissionsGuard } from '../auth/permissions.guard';
 import { PermissoesService } from './permissoes.service';
 import { CreatePermissoesDto } from './create-permissoes.dto';
 import { UpdatePermissaoDto } from './update-permissao.dto';
 
-@UseGuards(JwtAuthGuard, AdminGuard)
+@UseGuards(JwtAuthGuard, PermissionsGuard)
+@Permissions('permissoes', 'permissao')
 @Controller('permissoes')
 export class PermissoesController {
   constructor(private readonly permissoesService: PermissoesService) {}
