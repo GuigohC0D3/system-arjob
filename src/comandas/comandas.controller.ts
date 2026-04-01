4995b4418503afae405ad349adaad41b547f1ae2
Guilherme Ancheschi Werneck Pereira <guilhermewerneckpereira11@hotmail.com>
2026-04-01 09:57:14 -0300

    ✨ feat(master): Aplica controle de permissões no módulo de comandas.

--- src/comandas/comandas.controller.ts
+++ src/comandas/comandas.controller.ts
@@ -9,12 +9,14 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { AdminGuard } from '../auth/admin.guard';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Permissions } from '../auth/permissions.decorator';
+import { PermissionsGuard } from '../auth/permissions.guard';
 import { ComandasService } from './comandas.service';
 import { CreateComandaDto, UpdateComandaDto } from './comandas.dto';
 
-@UseGuards(JwtAuthGuard, AdminGuard)
+@UseGuards(JwtAuthGuard, PermissionsGuard)
+@Permissions('comandas', 'comanda')
 @Controller('comandas')
 export class ComandasController {
   constructor(private readonly comandasService: ComandasService) {}
